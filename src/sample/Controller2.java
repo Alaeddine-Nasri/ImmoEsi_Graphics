c453803d995cbe0842f04ff365618700027ff25d
Alaeddine <ia_nasri@esi.dz>
2020-01-22 23:04:10 +0100

    Making second scene ,Adminpage, without buttons to make it

---
+++ src/sample/Controller2.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller2 {
+    private Main main;
+    public Stage secondstage;
+    @FXML
+    private void goAdminstration() throws Exception {
+      //  main.showadministration(secondstage);
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,11 +13,16 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("proto.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 950, 560));
         primaryStage.show();
     }
 
+    public static void showadministration() throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation (Main.class.getResource("Adminstration.fxml"));
+        //BorderPane administration = loader.load();
 
+    }
     public static void main(String[] args) {
         launch(args);
     }
